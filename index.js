9e179a0bb159cff63806cd207ea0e651ff22e64f
Edalia <edaliamk@gmail.com>
2023-03-03 16:32:32 +0300

    commit 2
    add issue functionality and display issues stored in Local Storage

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 let issue_click = document.getElementById("issue-click");
-let issue = {}
-let issues = []
-let issue_text = document.getElementById("issue-text")
+let addIssue_header = document.getElementById("addIssue-header");
+let issue = {};
+let issues = [];
+let issue_text = document.getElementById("issue-text");
+const issues_cardBody = document.getElementById("issues-card-body");
 
-issue_click.addEventListener('click', addIssue())
+fetch_issues();
 
-function addIssue(){
+issue_click.addEventListener('click', function(e){
+
     e.preventDefault();
 
     const description = document.getElementById('description').value;
     const priority = document.getElementById('priority').value;
     const assigned_to = document.getElementById('assigned-to').value;
 
+    if(description && priority && assigned_to){
         issue = {
             issue_id: generate_issueID(),
             issue_description:description,
@@ -19,17 +23,30 @@ function addIssue(){
             issue_assign: assigned_to
         };
     
+        issues.push(issue)
+
         // Store the object into storage
-        // let storeObj = localStorage.setItem("issue", JSON.stringify(issue));
+        localStorage.setItem("issue", JSON.stringify(issues));
      
-        if(issue){
-             alert("Issue was addedd successfully");
+        if(localStorage.hasOwnProperty("issue")){
+            addIssue_header.innerHTML = "The issue was added successfully";
+            issues_cardBody.innerHTML = " ";
+            addIssue_header.className = "alert alert-success"
+            fetch_issues();
+
+            
         }else{
-             alert("There was an error in recording the issue");
+            addIssue_header.innerHTML = "There was an error in recording the issue";
+            addIssue_header.className = "alert alert-danger"
         }
     
-        console.log(issue);
-}
+        console.log(issues);
+    }else{
+        addIssue_header.innerHTML = "A field was left blank";
+        addIssue_header.className = "alert alert-danger"
+    }
+
+})
 
 //issue ID generator
 function generate_issueID(){
@@ -48,3 +65,51 @@ for(let i=0; i<10; i++){
 return String(issue_id);
 
 }
+
+
+function fetch_issues(){
+
+    let issues = JSON.parse(localStorage.getItem("issue"));
+
+    if(issues){
+        
+
+        for(let i=0; i<issues.length; i++){
+            issues_cardBody.innerHTML +=  `                    
+    
+            <div class="row">
+                <p> Issue id: ${issues[i].issue_id}</p>
+            </div>
+            <div>
+                <b><p id="issue-text">${issues[i].issue_description}</p></b>
+            </div>
+        
+            <div class="row">
+                <p id="priority-text">
+                    <i class="bi bi-clock"></i>${issues[i].issue_priority} 
+                </p>
+                &nbsp&nbsp
+                <p id="assign-text">
+                    <i class="bi bi-person-fill"></i>${issues[i].issue_assign}
+                </p>    
+            </div>
+            <button class="btn btn-warning"  >close</button>
+            <button class="btn btn-danger" onclick="">delete</button>
+            <hr>
+            `
+            ;
+        }
+    }else{
+        issues_cardBody.innerHTML = `
+        <div class="row">
+            <p class="alert alert-primary"> There are no reported issues</p>
+        </div>`
+    }
+}
+
+
+// function deleteIssue(i){
+//     issues.pop(issues[i]);
+
+
+// }
